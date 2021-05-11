b6f7b0d646fbd04fc8d186a001a18a73ad579150
John Doe <Sel7Nik@gmail.com>
2021-05-12 00:37:17 +0300

    React JS Lesson 07  11may

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+// import React from 'react';
 import './App.css';
 
-function App() {
-  return (
+// function App() {
+  const  App = () => {
+  return <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>HTML</h1> 
+    <ul className="react__list">
+      <li className="react__item">list 01</li>
+      <li className="react__item">list 02</li>
+      <li className="react__item">list 03</li>
+      <li className="react__item">list 04</li>
+    </ul>
     </div>
-  );
+      <Abb></Abb>
+    </>
+  
 }
+  const  Abb = () => {
+  return  <>
+            <div className="Abb">
+            <h1 className="h1" >Abb</h1> 
+            <p>second compponent</p>
+            </div>
+          </>
+}
+
+
+
 
 export default App;
