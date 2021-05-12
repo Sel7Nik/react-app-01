bbb445864700c487be1b19597f6057db32594be0
John Doe <Sel7Nik@gmail.com>
2021-05-12 13:46:52 +0300

    react Lesson 11 grid

--- src/Abb.js
+++
@@ -1,9 +0,0 @@
- const  Abb = () => {
-  return  <>
-            <div className="Abb">
-            <h1 className="h1" >Abb</h1> 
-            <p>second compponent</p>
-            </div>
-          </>
-}
-export default Abb;
