38e20353c19608d77ae976dd54db410a8f6c0938
Sel7Nik <78795757+Sel7Nik@users.noreply.github.com>
2021-05-15 19:44:58 +0300

    react Lesson page Dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,7 +1,20 @@
 import s from './Dialogs.module.css';
 
 const Dialogs = (p) => {
-  return <div>Dialogs</div>;
+  return (
+    <div className={s.dialogs}>
+      <div className={s.dialogs__items}>
+        <div className={s.dialog + ' ' + s.active}>DimDimitch</div>
+        <div className={s.dialog}>DimVovich</div>
+        <div className={s.dialog}>DimSanytch</div>
+      </div>
+      <div className={s.messanges}>
+        <div className={s.message}>Hi</div>
+        <div className={s.message}>How are you</div>
+        <div className={s.message}>i am fine</div>
+      </div>
+    </div>
+  );
 };
 
 export default Dialogs;
