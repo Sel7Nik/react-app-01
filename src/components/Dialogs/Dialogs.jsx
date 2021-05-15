085af80829826c4ad3a4b98265dde83be6d49b66
Sel7Nik <78795757+Sel7Nik@users.noreply.github.com>
2021-05-15 21:31:48 +0300

    react Lesson22 route exact

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,12 +1,28 @@
+import { NavLink } from 'react-router-dom';
 import s from './Dialogs.module.css';
 
 const Dialogs = (p) => {
   return (
     <div className={s.dialogs}>
       <div className={s.dialogs__items}>
-        <div className={s.dialog + ' ' + s.active}>DimDimitch</div>
-        <div className={s.dialog}>DimVovich</div>
-        <div className={s.dialog}>DimSanytch</div>
+        <div className={s.dialog + ' ' + s.active}>
+          <NavLink to="/dialogs/1">DimDimitch</NavLink>
+        </div>
+        <div className={s.dialog}>
+          <NavLink to="/dialogs/2">DimVovich</NavLink>
+        </div>
+        <div className={s.dialog}>
+          <NavLink to="/dialogs/3">NikSanytch</NavLink>
+        </div>
+        <div className={s.dialog}>
+          <NavLink to="/dialogs/4">OlegSanytch</NavLink>
+        </div>
+        <div className={s.dialog}>
+          <NavLink to="/dialogs/5">PetrSanytch</NavLink>
+        </div>
+        <div className={s.dialog}>
+          <NavLink to="/dialogs/5">Olegovich</NavLink>
+        </div>
       </div>
       <div className={s.messanges}>
         <div className={s.message}>Hi</div>
