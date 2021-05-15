043928f6160aeb302b3b8543c7174cb5ce615e23
Sel7Nik <78795757+Sel7Nik@users.noreply.github.com>
2021-05-15 23:37:12 +0300

    react Lesson 23 refactoring messages

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,33 +1,42 @@
 import { NavLink } from 'react-router-dom';
 import s from './Dialogs.module.css';
 
+const DialogItem = (p) => {
+  let path = '/dialogs/' + p.id;
+  return (
+    <div className={s.dialog}>
+      <NavLink to={path}> {p.name}</NavLink>
+    </div>
+  );
+};
+
+const Message = (p) => {
+  return (
+    <>
+      <div className={s.message}>{p.message}</div>
+    </>
+  );
+};
+
 const Dialogs = (p) => {
   return (
     <div className={s.dialogs}>
       <div className={s.dialogs__items}>
-        <div className={s.dialog + ' ' + s.active}>
+        {/* <div className={s.dialog + ' ' + s.active}>
           <NavLink to="/dialogs/1">DimDimitch</NavLink>
-        </div>
-        <div className={s.dialog}>
-          <NavLink to="/dialogs/2">DimVovich</NavLink>
-        </div>
-        <div className={s.dialog}>
-          <NavLink to="/dialogs/3">NikSanytch</NavLink>
-        </div>
-        <div className={s.dialog}>
-          <NavLink to="/dialogs/4">OlegSanytch</NavLink>
-        </div>
-        <div className={s.dialog}>
-          <NavLink to="/dialogs/5">PetrSanytch</NavLink>
-        </div>
-        <div className={s.dialog}>
-          <NavLink to="/dialogs/5">Olegovich</NavLink>
-        </div>
+        </div> */}
+
+        <DialogItem name="DimDimitch" id="1" />
+        <DialogItem name="DimVovich" id="2" />
+        <DialogItem name="NikSanytch" id="3" />
+        <DialogItem name="OlegSanytch" id="4" />
+        <DialogItem name="PetrSanytch" id="5" />
+        <DialogItem name="Olegovich" id="6" />
       </div>
       <div className={s.messanges}>
-        <div className={s.message}>Hi</div>
-        <div className={s.message}>How are you</div>
-        <div className={s.message}>i am fine</div>
+        <Message message="Hi" />
+        <Message message="How are you" />
+        <Message message="I am fine" />
       </div>
     </div>
   );
