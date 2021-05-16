7494c2fdeb13e7eed3b4528edc9baef02ea505c4
Sel7Nik <78795757+Sel7Nik@users.noreply.github.com>
2021-05-16 14:27:22 +0300

    react Lesson 24 UI-BLL

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -19,6 +19,27 @@ const Message = (p) => {
 };
 
 const Dialogs = (p) => {
+  let dialogsData = [
+    { id: 1, name: 'DimDimitch' },
+    { id: 2, name: 'DimVovich' },
+    { id: 3, name: 'NikSanytch' },
+    { id: 4, name: 'OlegSanytch' },
+    { id: 5, name: 'PetrSanytch' },
+    { id: 6, name: 'Olegovich' },
+  ];
+
+  let messagesData = [
+    { id: 1, message: 'Hi' },
+    { id: 2, message: 'Hello' },
+    { id: 3, message: 'How are you' },
+    { id: 4, message: 'I am fine' },
+    { id: 5, message: 'I am fine too' },
+    { id: 6, message: 'OhOhOh' },
+  ];
+
+  let dD = dialogsData;
+  let ms = messagesData;
+
   return (
     <div className={s.dialogs}>
       <div className={s.dialogs__items}>
@@ -26,17 +47,19 @@ const Dialogs = (p) => {
           <NavLink to="/dialogs/1">DimDimitch</NavLink>
         </div> */}
 
-        <DialogItem name="DimDimitch" id="1" />
-        <DialogItem name="DimVovich" id="2" />
-        <DialogItem name="NikSanytch" id="3" />
-        <DialogItem name="OlegSanytch" id="4" />
-        <DialogItem name="PetrSanytch" id="5" />
-        <DialogItem name="Olegovich" id="6" />
+        <DialogItem name={dD[0].name} id={dD[0].id} />
+        <DialogItem name={dD[1].name} id={dD[1].id} />
+        <DialogItem name={dD[2].name} id={dD[2].id} />
+        <DialogItem name={dD[3].name} id={dD[3].id} />
+        <DialogItem name={dD[4].name} id={dD[4].id} />
+        <DialogItem name={dD[5].name} id={dD[5].id} />
       </div>
       <div className={s.messanges}>
-        <Message message="Hi" />
-        <Message message="How are you" />
-        <Message message="I am fine" />
+        <Message message={ms[0].message} />
+        <Message message={ms[1].message} />
+        <Message message={ms[2].message} />
+        <Message message={ms[3].message} />
+        <Message message={ms[4].message} />
       </div>
     </div>
   );
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,6 +3,15 @@ import Post from './Post/Post.jsx';
 import s from './MyPosts.module.css';
 
 const MyPosts = () => {
+  let postsData = [
+    { id: 1, message: 'Hi, how are you Ok', likeCount: '23' },
+    { id: 2, message: 'It is my first post', likeCount: '0' },
+    { id: 3, message: 'It is my second post', likeCount: '32' },
+    { id: 4, message: 'It is my eleven post', likeCount: '99' },
+  ];
+
+  let pD = postsData;
+
   return (
     <>
       <div className={s.postsBlock}>
@@ -11,17 +20,32 @@ const MyPosts = () => {
           <textarea></textarea>
           <button>Add post</button>
         </div>
+
         <div className={s.posts}>
-          <Post name="Nik" age="33" message="Hi, how are you" likeCount="23" />
+          <Post
+            name="Nik"
+            age="33"
+            message={pD[0].message}
+            likeCount={pD[0].likeCount}
+          />
           <Post
             name="Pik"
             age="44"
-            message="It's my first post"
-            likeCount="0"
+            message={pD[1].message}
+            likeCount={pD[1].likeCount}
           />
-          <Post name="Sip" age="22" message="" likeCount="32" />
-          <Post likeCount="99" />
-          <Post likeCount="7" />
+          <Post
+            name="Sip"
+            age="22"
+            message={pD[2].message}
+            likeCount={pD[2].likeCount}
+          />
+          <Post
+            name="Helen"
+            age="21"
+            message={pD[3].message}
+            likeCount={pD[3].likeCount}
+          />
         </div>
       </div>
     </>
