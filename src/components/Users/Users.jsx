95e9145e5ad2c59e471f5e300452510c6492176b
Sel7Nik <78795757+Sel7Nik@users.noreply.github.com>
2021-05-27 18:59:42 +0300

    react Lesson 51 Users API

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,41 +1,18 @@
+// import axios from 'axios';
 import css from './users.module.css';
+import * as axios from 'axios';
+import userPhoto from '../../assets/images/unnamed.jpg';
 
 let Users = (props) => {
   if (props.users.length === 0) {
-    props.setUsers([
-      {
-        id: 1,
-        photoUrl: 'https://cs6.pikabu.ru/avatars/237/v237661.jpg',
-        followed: true,
-        fullName: 'DmitryHi',
-        status: 'I am boss',
-        location: { city: 'Minsk', country: 'Belarus' },
-      },
-      {
-        id: 2,
-        photoUrl: 'https://cs6.pikabu.ru/avatars/237/v237661.jpg',
-        followed: false,
-        fullName: 'Nikolay',
-        status: 'I am boss',
-        location: { city: 'Kiev', country: 'Ukraina' },
-      },
-      {
-        id: 3,
-        photoUrl: 'https://cs6.pikabu.ru/avatars/237/v237661.jpg',
-        followed: false,
-        fullName: 'Sergei',
-        status: 'I am boss',
-        location: { city: 'Moscow', country: 'Russia' },
-      },
-      {
-        id: 4,
-        photoUrl: 'https://cs6.pikabu.ru/avatars/237/v237661.jpg',
-        followed: true,
-        fullName: 'Pavel',
-        status: 'I am boss',
-        location: { city: 'Paris', country: 'Franch' },
-      },
-    ]);
+    const response = axios.get(
+      'https://social-network.samuraijs.com/api/1.0/users'
+    );
+
+    response.then((response) => {
+      debugger;
+      props.setUsers(response.data.items);
+    });
   }
 
   return (
@@ -44,7 +21,11 @@ let Users = (props) => {
         <div key={u.id}>
           <span>
             <div>
-              <img src={u.photoUrl} alt="logo" className={css.userPhoto} />
+              <img
+                src={u.photos.small != null ? u.photos.small : userPhoto}
+                alt="logo"
+                className={css.userPhoto}
+              />
             </div>
             <div>
               {u.followed ? (
@@ -69,12 +50,12 @@ let Users = (props) => {
 
           <span>
             <span>
-              <div>{u.fullName}</div>
+              <div>{u.name}</div>
               <div>{u.status}</div>
             </span>
             <span>
-              <div>{u.location.city}</div>
-              <div>{u.location.country}</div>
+              <div>{'u.location.city'}</div>
+              <div>{'u.location.country'}</div>
             </span>
           </span>
         </div>
