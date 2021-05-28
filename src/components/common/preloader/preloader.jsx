862d2e02344df704881a990e063990dd577ffb5f
Sel7Nik <78795757+Sel7Nik@users.noreply.github.com>
2021-05-28 23:27:46 +0300

    react Lesson 57 Preloader, loader-gif, isFetching

---
+++ src/components/common/preloader/preloader.jsx
@@ -0,0 +1,10 @@
+import preloader from './Spinner-1s-200px.svg';
+
+let Preloader = (props) => {
+  return (
+    <div>
+      <img src={preloader} />
+    </div>
+  );
+};
+export default Preloader;
