5da4e62a9e86051fca092109aa14fe93ce63f0c3
Sel7Nik <78795757+Sel7Nik@users.noreply.github.com>
2021-05-31 01:18:31 +0300

    L 63 DAL, axios.create

---
+++ src/api/api.js
@@ -0,0 +1,19 @@
+import * as axios from 'axios';
+
+const instance = axios.create({
+  withCredentials: true,
+  baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+  headers: {
+    'API-KEY': '4438de1e-db1f-4295-ad92-efa0743a311e',
+  },
+});
+
+export const usersAPI = {
+  getUsers(currentPage = 1, pageSize = 10) {
+    return instance
+      .get(`users?page=${currentPage}&count=${pageSize}`)
+      .then((response) => {
+        return response.data;
+      });
+  },
+};
