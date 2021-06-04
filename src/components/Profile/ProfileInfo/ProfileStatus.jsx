cb804cd46864a50528f9feb1c972baed81b8cfa6
Sel7Nik <78795757+Sel7Nik@users.noreply.github.com>
2021-06-04 21:31:22 +0300

    L 74 componentDidUpdate

--- src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -21,6 +21,17 @@ class ProfileStatus extends React.Component {
   onStatusChange = (e) => {
     this.setState({ status: e.currentTarget.value });
   };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevProps.status !== this.props.status) {
+      this.setState({
+        status: this.props.status,
+      });
+    }
+
+    console.log('componentDidUpdate');
+  }
+
   render() {
     return (
       <>
