5ed6513f4867c81eb88bb58e4d0d652b024ae0d1
Sel7Nik <78795757+Sel7Nik@users.noreply.github.com>
2021-06-06 00:02:07 +0300

    add form on login page

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,4 +1,22 @@
 const Login = (props) => {
-  return <h1>LOGIN</h1>;
+  return (
+    <div>
+      <h1>LOGIN</h1>
+      <form>
+        <div>
+          <input placeholder="Login" />
+        </div>
+        <div>
+          <input placeholder="Password" />
+        </div>
+        <div>
+          <input ptype="checkbox" /> remember me
+        </div>
+        <div>
+          <button>Log in</button>
+        </div>
+      </form>
+    </div>
+  );
 };
 export default Login;
