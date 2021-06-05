0c1876d75484fc708037116a6c02e8ed6ffd35fe
Sel7Nik <78795757+Sel7Nik@users.noreply.github.com>
2021-06-06 00:12:13 +0300

    component LoginForm

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,21 +1,27 @@
+const LoginForm = (props) => {
+  return (
+    <form>
+      <div>
+        <input placeholder="Login" />
+      </div>
+      <div>
+        <input placeholder="Password" />
+      </div>
+      <div>
+        <input type="checkbox" /> remember me
+      </div>
+      <div>
+        <button>Log in</button>
+      </div>
+    </form>
+  );
+};
+
 const Login = (props) => {
   return (
     <div>
       <h1>LOGIN</h1>
-      <form>
-        <div>
-          <input placeholder="Login" />
-        </div>
-        <div>
-          <input placeholder="Password" />
-        </div>
-        <div>
-          <input ptype="checkbox" /> remember me
-        </div>
-        <div>
-          <button>Log in</button>
-        </div>
-      </form>
+      <LoginForm />
     </div>
   );
 };
