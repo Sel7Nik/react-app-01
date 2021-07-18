46e249d74ca16cd7f2e5f5ccce28144e5b7faaa4
John Doe <Sel7Nik@gmail.com>
2021-07-18 14:44:41 +0300

    L51 Users API

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,45 +1,14 @@
+import * as axios from 'axios';
 import styles from './users.module.css';
+import userPhoto from '../../images/avatar-anonymous-face.png';
 
 let Users = (props) => {
   if (props.users.length === 0) {
-    props.setUsers([
-      {
-        id: 1,
-        photoUrl:
-          'https://i.mycdn.me/i?r=AzEPZsRbOZEKgBhR0XGMT1Rk_21ZlSXUHM6T4sogJpp3vqaKTM5SRkZCeTgDn6uOyic',
-        followed: false,
-        fullName: 'Dmitry',
-        status: 'I am a boss',
-        location: { city: 'Minsk', country: 'Belarus' },
-      },
-      {
-        id: 2,
-        photoUrl:
-          'https://i.mycdn.me/i?r=AzEPZsRbOZEKgBhR0XGMT1Rk_21ZlSXUHM6T4sogJpp3vqaKTM5SRkZCeTgDn6uOyic',
-        followed: false,
-        fullName: 'Alex',
-        status: 'I am a Big boss',
-        location: { city: 'London', country: 'GB' },
-      },
-      {
-        id: 3,
-        photoUrl:
-          'https://i.mycdn.me/i?r=AzEPZsRbOZEKgBhR0XGMT1Rk_21ZlSXUHM6T4sogJpp3vqaKTM5SRkZCeTgDn6uOyic',
-        followed: true,
-        fullName: 'Nik Name',
-        status: 'I am a King',
-        location: { city: 'Moscow', country: 'Russia' },
-      },
-      {
-        id: 4,
-        photoUrl:
-          'https://i.mycdn.me/i?r=AzEPZsRbOZEKgBhR0XGMT1Rk_21ZlSXUHM6T4sogJpp3vqaKTM5SRkZCeTgDn6uOyic',
-        followed: false,
-        fullName: 'Sem Sim',
-        status: 'I am a monstro',
-        location: { city: 'Kiev', country: 'Ukraina' },
-      },
-    ]);
+    axios
+      .get('https://social-network.samuraijs.com/api/1.0/users')
+      .then((response) => {
+        props.setUsers(response.data.items);
+      });
   }
 
   return (
@@ -49,7 +18,7 @@ let Users = (props) => {
           <span>
             <div>
               <img
-                src={user.photoUrl}
+                src={user.photos.small != null ? user.photos.small : userPhoto}
                 className={styles.userPhoto}
                 alt="avatar"
               />
@@ -76,12 +45,12 @@ let Users = (props) => {
           </span>
           <span>
             <span>
-              <div>{user.fullName}</div>
+              <div>{user.name}</div>
               <div>{user.status}</div>
             </span>
             <span>
-              <div>{user.location.city}</div>
-              <div>{user.location.country}</div>
+              <div>"user.location.city"</div>
+              <div>"user.location.country"</div>
             </span>
           </span>
         </div>
