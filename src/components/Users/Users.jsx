6b4e0cee1c4f01bfbcfe8a43d28c042bb7c71616
John Doe <Sel7Nik@gmail.com>
2021-07-18 15:58:06 +0300

    L53 getUsers in constructor

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,15 +4,14 @@ import styles from './users.module.css';
 import userPhoto from '../../images/avatar-anonymous-face.png';
 
 class Users extends React.Component {
-  getUsers = () => {
-    if (this.props.users.length === 0) {
-      axios
-        .get('https://social-network.samuraijs.com/api/1.0/users')
-        .then((response) => {
-          this.props.setUsers(response.data.items);
-        });
-    }
-  };
+  constructor(props) {
+    super(props);
+    axios
+      .get('https://social-network.samuraijs.com/api/1.0/users')
+      .then((response) => {
+        this.props.setUsers(response.data.items);
+      });
+  }
 
   render() {
     return (
