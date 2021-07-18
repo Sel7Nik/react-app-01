3c7aeee58f807b1a1890869035d10c6fea6c87cc
John Doe <Sel7Nik@gmail.com>
2021-07-18 15:15:38 +0300

    L52 getUsers & button

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,16 +3,19 @@ import styles from './users.module.css';
 import userPhoto from '../../images/avatar-anonymous-face.png';
 
 let Users = (props) => {
-  if (props.users.length === 0) {
-    axios
-      .get('https://social-network.samuraijs.com/api/1.0/users')
-      .then((response) => {
-        props.setUsers(response.data.items);
-      });
-  }
+  let getUsers = () => {
+    if (props.users.length === 0) {
+      axios
+        .get('https://social-network.samuraijs.com/api/1.0/users')
+        .then((response) => {
+          props.setUsers(response.data.items);
+        });
+    }
+  };
 
   return (
     <>
+      <button onClick={getUsers}>GET USERS</button>
       {props.users.map((user) => (
         <div key={user.id}>
           <span>
