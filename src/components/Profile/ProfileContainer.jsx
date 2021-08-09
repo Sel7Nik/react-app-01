0156448ef1c8f70904432e4d0a79c771dffcff70
Sel7Nik <78795757+Sel7Nik@users.noreply.github.com>
2021-08-10 01:26:31 +0300

    L 67 redux-thunk

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -1,19 +1,14 @@
 import React from 'react';
 import Profile from './Profile';
-import * as axios from 'axios';
 import { connect } from 'react-redux';
-import { setUserProfile } from '../../redux/profile-reducer.js';
+import { getUserProfile } from '../../redux/profile-reducer.js';
 import { withRouter } from 'react-router-dom';
 
 class ProfileContainer extends React.Component {
   componentDidMount() {
     let userId = this.props.match.params.userId;
     if (!userId) userId = 2;
-    axios
-      .get(`https://social-network.samuraijs.com/api/1.0/profile/` + userId)
-      .then((response) => {
-        this.props.setUserProfile(response.data);
-      });
+    this.props.getUserProfile(userId);
   }
   render() {
     return <Profile {...this.props} profile={this.props.profile} />;
@@ -23,7 +18,7 @@ let mapStateToProps = (state) => ({
   profile: state.profilePage.profile,
 });
 
-let mapDispatchToProps = { setUserProfile };
+let mapDispatchToProps = { getUserProfile };
 
 let WithUrlDataContainerComponent = withRouter(ProfileContainer);
 
