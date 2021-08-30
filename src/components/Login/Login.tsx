6ff52ccfcb5968eeb948196fab0ee63cfcb14d4c
Sel7Nik <78795757+Sel7Nik@users.noreply.github.com>
2021-08-30 22:16:35 +0300

    L2-08-22  LoginForm  LoginReduxForm

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,14 +1,19 @@
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
-import { reduxForm } from 'redux-form';
+import { InjectedFormProps, reduxForm } from 'redux-form';
 import { login } from '../../redux/auth-reducer';
+import { AppStateType } from '../../redux/redux-store';
 import { maxLengthCreator, required } from '../../utils/validators/validators';
 import { createField, Input } from '../common/FormsControls/FormsControls';
 import css from './../common/FormsControls/FormsControls.module.css';
 
 const maxLength50 = maxLengthCreator(50);
 //!   LoginForm
-const LoginForm = ({ handleSubmit, error, captchaUrl }) => {
+
+type LoginFormOwnProps = {
+  captchaUrl: string | null
+}
+const LoginForm: React.FC<InjectedFormProps<LoginFormValuesType, LoginFormOwnProps> & LoginFormOwnProps> = ({ handleSubmit, error, captchaUrl }) => {
   return (
     <form onSubmit={handleSubmit}>
       {createField('Email', 'email', [required, maxLength50], Input)}
@@ -39,13 +44,31 @@ const LoginForm = ({ handleSubmit, error, captchaUrl }) => {
   );
 };
 
-const LoginReduxForm = reduxForm({
+const LoginReduxForm = reduxForm<LoginFormValuesType, LoginFormOwnProps>({
   form: 'login',
 })(LoginForm);
 
+type MapStatePropsType = {
+  captchaUrl: string | null
+  isAuth: boolean
+}
+
+type MapDispatchPropsType = {
+  login: (email: string, password: string, rememberMe: boolean, captcha: string) => void
+}
+
+
+type LoginFormValuesType = {
+  captcha: string
+  rememberMe: boolean
+  password: string
+  email: string
+}
+
+
 //!   Login
-const Login = (props) => {
-  const onSubmit = (formData) => {
+const Login: React.FC<MapStatePropsType & MapDispatchPropsType> = (props) => {
+  const onSubmit = (formData: LoginFormValuesType) => {
     props.login(
       formData.email,
       formData.password,
@@ -65,7 +88,7 @@ const Login = (props) => {
     </div>
   );
 };
-const mapStateToProps = (state) => ({
+const mapStateToProps = (state: AppStateType) => ({
   captchaUrl: state.auth.captchaUrl,
   isAuth: state.auth.isAuth,
 });
