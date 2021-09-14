fe286fe4be0d5385570d5b2126748cbf3827ce9b
Sel7Nik <78795757+Sel7Nik@users.noreply.github.com>
2021-09-14 23:37:33 +0300

    L2-18-31 Chat Messages AddMessagesForm

--- src/pages/Chat/ChatPage.tsx
+++ src/pages/Chat/ChatPage.tsx
@@ -3,8 +3,31 @@ import { FC } from "react"
 const ChatPage: FC = () => {
   // export const ChatPage: React.FC=()=>{
   return <div>
-    Chat
+    <Chat />
   </div>
 }
+
+const Chat: FC = () => {
+  return <div>
+    <Messages />
+    <AddMessagesForm />
+  </div>
+}
+const Messages: FC = () => {
+  return <div>
+
+  </div>
+}
+const AddMessagesForm: FC = () => {
+  return <div>
+    <textarea>text message</textarea>
+    <button> S E N D </button>
+  </div>
+}
+
+
+
+
+
 
 export default ChatPage
