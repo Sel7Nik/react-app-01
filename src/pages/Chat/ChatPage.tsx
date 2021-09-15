4ead5552e48b0813dbd3aea83edd4aa70cdc4784
Sel7Nik <78795757+Sel7Nik@users.noreply.github.com>
2021-09-15 16:44:27 +0300

    L2-18-43 const ws = new WebSocket

--- src/pages/Chat/ChatPage.tsx
+++ src/pages/Chat/ChatPage.tsx
@@ -1,5 +1,8 @@
 import { FC } from "react"
 // import React from 'react'
+
+const ws = new WebSocket('wss://social-network.samuraijs.com/handlers/ChatHandler.ashx')
+
 const ChatPage: FC = () => {
   // export const ChatPage: React.FC=()=>{
   return <div>
@@ -14,14 +17,40 @@ const Chat: FC = () => {
   </div>
 }
 const Messages: FC = () => {
-  return <div>
-
+  const messages: Array<number> = [1, 2, 3, 4]
+  return <div style={{ height: "480px", overflowY: 'auto' }}>
+    {messages.map((m: any) =>
+      <Message />)}
+    {messages.map((m: any) =>
+      <Message />)}
+    {messages.map((m: any) =>
+      <Message />)}
   </div>
 }
+
+const Message: FC = () => {
+  const message = {
+    url: 'https://via.placeholder.com/30',
+    author: 'Nik Semenov',
+    text: 'Hello, My friends'
+  }
+  return <div>
+    <img src={message.url} alt="avatar" /><b>{message.author}</b>
+
+    <br />
+    {message.text}
+    <hr />
+  </div>
+}
+
 const AddMessagesForm: FC = () => {
   return <div>
-    <textarea>text message</textarea>
-    <button> S E N D </button>
+    <div>
+      <textarea>text message</textarea>
+    </div>
+    <div>
+      <button> S E N D </button>
+    </div>
   </div>
 }
 
