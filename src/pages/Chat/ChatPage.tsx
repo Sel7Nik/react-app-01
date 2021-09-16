d73bc691ae87d9df90cc77a39d456c8be22cbfc1
Sel7Nik <78795757+Sel7Nik@users.noreply.github.com>
2021-09-16 21:40:15 +0300

    L2-19-26 reconnect WebSocket

--- src/pages/Chat/ChatPage.tsx
+++ src/pages/Chat/ChatPage.tsx
@@ -19,16 +19,27 @@ const Chat: FC = () => {
   const [wsChannel, setWsChannel] = useState<WebSocket | null>(null)
 
   useEffect(() => {
+    let ws: WebSocket
+
+    const closeHandler = () => {
+      setTimeout(createChannel, 3000)
+    }
+
     function createChannel() {
-      setWsChannel(new WebSocket('wss://social-network.samuraijs.com/handlers/ChatHandler.ashx'))
+      if (ws !== null) {
+        ws.removeEventListener('close', closeHandler)
+      }
+
+
+      ws = new WebSocket('wss://social-network.samuraijs.com/handlers/ChatHandler.ashx')
+      ws.addEventListener('close', closeHandler)
+      setWsChannel(ws)
     }
     createChannel();
   }, [])
 
   useEffect(() => {
-    wsChannel?.addEventListener('close', () => {
-      console.log('CLOSE WS')
-    })
+
   }, [wsChannel])
 
   return <div>
